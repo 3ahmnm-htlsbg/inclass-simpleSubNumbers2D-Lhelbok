4867d060a59f51d53c6c7f10d54cdf7d2cbaaa79
LH03 <lea.helbok03@htl-salzburg.ac.at>
2019-12-16 19:20:20 +0100

    code the function of the fuctionality of the GUI

---
+++ Assets/MyGame/Scripts/CalculateNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalculateNumbers : MonoBehaviour
+{
+    public Text result;
+    public InputField valueA;
+    public InputField valueB;
+    public Button btn_reset;
+    public Button btn_calculate;
+
+    public void CalcResult()
+    {
+        result.text = ComputeNumbers().ToString();
+        valueA.interactable = false;
+        valueB.interactable = false;
+        btn_calculate.interactable = false;
+        btn_reset.interactable = true;
+    }
+
+    public void ResetResult()
+    {
+        valueA.text = "0";
+        valueB.text = "0";
+        valueA.interactable = true;
+        valueB.interactable = true;
+        btn_calculate.interactable = true;
+        btn_reset.interactable = false;
+        result.text = "0";
+    }
+
+    private int ComputeNumbers()
+    {
+        int functionResult = int.Parse(valueA.text) - int.Parse(valueB.text);
+        return functionResult;
+    }
+}
